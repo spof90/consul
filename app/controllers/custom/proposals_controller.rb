887dcf5eff989ae0d020c238fcc09693fcaa663f
itw-smart <tobias@itw-smartbusiness.de>
2020-11-26 11:09:16 +0100

    proposals_controller redirect

--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -3,7 +3,7 @@ require_dependency Rails.root.join("app", "controllers", "proposals_controller")
 class ProposalsController
   include ProposalsHelper
 
-  before_action(except: [:json_data]) {authenticate_user!}
+  before_action(except: [:json_data, :index, :show, :map, :summary]) {authenticate_user!}
   before_action :redirect, only: :index
 
   load_and_authorize_resource except: :json_data
@@ -11,7 +11,11 @@ class ProposalsController
   skip_authorization_check only: :json_data
 
   def redirect
-    if (!params[:search].present? || !Tag.category_names.include?(params[:search])) &&
+    if (params[:search].present?) && (!current_user.present? || !(current_user.moderator? || current_user.administrator?)) then
+        params[:project] = params[:search]
+    end
+    
+    if (!params[:project].present? || !Tag.category_names.include?(params[:project])) &&
       (!current_user.present? || !(current_user.moderator? || current_user.administrator?)) then
         redirect_to "/"
     end
@@ -33,6 +37,7 @@ class ProposalsController
     load_selected
     load_featured
     remove_archived_from_order_links
+    take_only_by_tag_name
     @proposals_coordinates = all_proposal_map_locations
   end
 
@@ -48,4 +53,21 @@ class ProposalsController
     end
   end
 
-end
+  private
+    def take_only_by_tag_name
+      if params[:project].present?
+        @resources = @resources.proposals_by_category(params[:project])
+      end
+    end
+    
+    def all_active_proposals
+     	if params[:project]
+    		Proposal.published().not_retired().not_archived().proposals_by_category(params[:project])
+    	elsif params[:search]
+      	Proposal.published().not_retired().not_archived().search(params[:search])
+	    else
+	      Proposal.published().not_retired().not_archived().all
+	    end
+    end
+
+  end
